10181e0402ad220e97d59e5d600d9df896429b85
terfo1 <alisher.temirhan@gmail.com>
2026-03-27 20:06:06 +0500

    test

--- postman/playwright.config.ts
+++ postman/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./tests",
+  testDir: ".",
   timeout: 30_000,
   expect: {
     timeout: 10_000,
